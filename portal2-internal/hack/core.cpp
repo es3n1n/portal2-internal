89a0211fb5a033a2ab861e353caad02457ac508a
fiction <me@es3n.in>
2021-04-21 08:50:12 +0200

    feat(startup): awaiting for unload

--- portal2-internal/hack/core.cpp
+++ portal2-internal/hack/core.cpp
@@ -1,5 +1,6 @@
 #include "hack.h"
 #include <thread>
+#include <chrono>
 
 
 namespace hack {
@@ -7,6 +8,17 @@ namespace hack {
 		void _initial_routine( ) {
 			util::logger::startup( );
 			util::logger::info( "Initializing stuff" );
+
+
+			while ( !GetAsyncKeyState( VK_DELETE ) ) // @todo: std::condition_variable + input sys based on hooked wndproc
+				std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
+
+
+			// 
+			// unload
+			//
+			util::logger::info( "Panic key was pressed, bye" );
+			core::_shutdown( );
 		}
 
 		bool startup( ) {
