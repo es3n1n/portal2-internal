c1a501fca912a3d54720c46ea8a9d8439a74c2b4
fiction <me@es3n.in>
2021-04-21 10:49:37 +0200

    refactor(modules): remove debug lines

--- portal2-internal/sdk/portal.cpp
+++ portal2-internal/sdk/portal.cpp
@@ -5,8 +5,6 @@ namespace portal {
 	namespace modules {
 		void capture() {
 			client = util::mem::module_t( "client.dll" );
-
-			auto sig = client.find_pattern( "55 8B EC 51 56 8D 45 FC 50 8B F1 E8 ? ? ? ? 6A 04" );
 		}
 	}
 
