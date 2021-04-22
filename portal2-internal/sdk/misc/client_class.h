fd230521a2544c2cbadeb27cc1de1984939a432a
fiction <me@es3n.in>
2021-04-22 23:19:49 +0200

    feat(sdk): implement new misc stuff

---
+++ portal2-internal/sdk/misc/client_class.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "recv.h"
+
+
+class i_client_networkable;
+
+typedef i_client_networkable* ( *create_client_class_fn )( int entnum, int serialNum );
+typedef i_client_networkable* ( *create_event_fn )( );
+
+class c_clientclass {
+public:
+    create_client_class_fn m_create;
+    create_event_fn m_create_event;
+    char* m_network_name;
+    c_recv_table* m_recv_table;
+    c_clientclass* m_next;
+    int m_classid;
+};
