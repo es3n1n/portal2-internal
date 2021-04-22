7b7b98a888c2678918bb60159ab90e3d896ec705
fiction <me@es3n.in>
2021-04-23 00:14:13 +0200

    docs(bhop): add explanation

--- portal2-internal/hack/features/misc/bhop.cpp
+++ portal2-internal/hack/features/misc/bhop.cpp
@@ -3,10 +3,11 @@
 
 namespace hack::features::misc {
 	void bhop( c_usercmd* cmd ) {
-		if ( !( cmd->m_buttons & e_cmd_buttons::in_jump ) || 
-			g::localplayer->m_fFlags() & e_ent_flags::fl_onground )
-			return;
+		if ( !( cmd->m_buttons & e_cmd_buttons::in_jump ) || // space button isn't pressed
+			g::localplayer->m_fFlags() & e_ent_flags::fl_onground // we're on ground
+			)
+			return; // so we'll keep a space button to jump
 
-		cmd->m_buttons &= ~e_cmd_buttons::in_jump;
+		cmd->m_buttons &= ~e_cmd_buttons::in_jump; // remove jump button ( space button is pressed & we're in air ) 
 	}
 }
