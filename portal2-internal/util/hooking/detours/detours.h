e7a325bea9b485c0f4bc77864709734b94d02ce6
fiction <me@es3n.in>
2021-04-22 10:27:13 +0200

    docs(hooking): add outdated warn

--- portal2-internal/util/hooking/detours/detours.h
+++ portal2-internal/util/hooking/detours/detours.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "mh/minhook.h"
-#pragma comment(lib, "mh/minhook.lib")
+#pragma comment(lib, "mh/minhook.lib") // warn: minhook lib might be outdated, i don't care
 
 
 namespace util::hooking::detour {
