f6c1a337397e8e4ef1670f9b4e678c05275e9353
fiction <me@es3n.in>
2021-04-24 16:56:05 +0200

    refactor(menu): minor menu changes ( mostly codestyle )

--- portal2-internal/hack/menu/menu.cpp
+++ portal2-internal/hack/menu/menu.cpp
@@ -12,15 +12,17 @@ namespace hack::menu {
 		ImGui::SetNextWindowPos( ImGui::GetIO( ).DisplaySize * 0.5f, ImGuiCond_Once, ImVec2( 0.5f, 0.5f ) );
 		ImGui::SetNextWindowSize( ImVec2( 500, 450 ), ImGuiCond_Once );
 
-		if ( ImGui::Begin( "portal2-internal by es3n1n", &opened, ImGuiWindowFlags_NoCollapse ) ) {
-			ImGui::Checkbox( "bhop", &cfg::opts::bhop );
-			ImGui::Spacing( );
-			if ( ImGui::Button( "save" ) )
-				cfg::save( "config" );
-			ImGui::SameLine( );
-			if ( ImGui::Button( "load" ) )
-				cfg::read( "config" );
-		}
+		if ( !ImGui::Begin( "portal2-internal by es3n1n", &opened, ImGuiWindowFlags_NoCollapse ) )
+			return ImGui::End( );
+
+		ImGui::Checkbox( "bhop", &cfg::opts::bhop );
+		ImGui::Spacing( );
+		if ( ImGui::Button( "save" ) )
+			cfg::save( "config" );
+		ImGui::SameLine( );
+		if ( ImGui::Button( "load" ) )
+			cfg::read( "config" );
+
 		ImGui::End( );
 	}
 
