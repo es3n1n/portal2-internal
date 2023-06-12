0916f9ce2426923a193d7744edd4c79828572f16
Arsenii es3n1n <me@es3n.in>
2023-06-12 19:24:07 +0200

    #12: refactor(client_class): change callback types decl

--- portal2-internal/sdk/misc/client_class.hpp
+++ portal2-internal/sdk/misc/client_class.hpp
@@ -3,12 +3,12 @@
 
 class i_client_networkable;
 
-typedef i_client_networkable* (*create_client_class_fn)(int entnum, int serialNum);
-typedef i_client_networkable* (*create_event_fn)();
-
 class c_clientclass {
+    using create_class_fn = i_client_networkable*(int ent_num, int serial_num);
+    using create_event_fn = i_client_networkable*();
+
 public:
-    create_client_class_fn m_create;
+    create_class_fn m_create;
     create_event_fn m_create_event;
     char* m_network_name;
     c_recv_table* m_recv_table;
