bb908c6d9be1fbc1b845f86e7a0af9c6527e6a08
Igor Fedenko <kehrazybox@gmail.com>
2023-06-13 22:07:17 +0300

    refactor: use the standard library for constant definitions

--- portal2-internal/util/math/base.hpp
+++ portal2-internal/util/math/base.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <limits>
+#include <numbers>
+
 namespace util::math {
-    constexpr auto e = 2.71828182845904523536f;
-    constexpr auto pi = 3.14159265358979323846f;
+    constexpr double e = std::numbers::e;
+    constexpr auto pi = std::numbers::pi;
     constexpr auto radpi = 180.f / pi;
     constexpr auto sqrpi = pi * pi;
-    constexpr auto round_error = 0.5;
+    constexpr auto round_error = std::numeric_limits<double>::round_error();
 
     [[nodiscard]] inline double deg2rad(const double& x) {
         return x * (static_cast<double>(pi) / 180.0);
