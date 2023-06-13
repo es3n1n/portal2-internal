588f4a4712757196fa1a6383912ba5e01a5fb77a
Arsenii es3n1n <me@es3n.in>
2023-06-13 21:32:16 +0200

    #18: refactor(util): remove unused `MATH_PI` macro

--- portal2-internal/util/util.hpp
+++ portal2-internal/util/util.hpp
@@ -1,9 +1,8 @@
 #pragma once
-#define MATH_PI 3.14159265358979323846f
-
 #include "game/game.hpp"
 #include "hooking/hooking.hpp"
 #include "input/input.hpp"
 #include "logger/logger.hpp"
 #include "math/math.hpp"
 #include "mem/mem.hpp"
+#include "util/util.hpp"
